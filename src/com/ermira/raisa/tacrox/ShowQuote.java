6fd13503cd502cd51fe2f1df5cf137c0d5bddde1
raisaa <raisa_uku@live.com>
2013-10-03 03:20:38 +0200

    Fix warnings

--- src/com/ermira/raisa/tacrox/ShowQuote.java
+++ src/com/ermira/raisa/tacrox/ShowQuote.java
@@ -1,12 +1,10 @@
 package com.ermira.raisa.tacrox;
 
 import java.util.HashMap;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class ShowQuote extends Activity {
